29f37b4ffdb44f478f359025e9209df0a74bd549
Lizokbondarieva <lizokbondarieva@gmail.com>
2024-10-22 22:19:00 +0300

    laba1 punckt 4(4)

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Product
+{
+    // Властивості для назви товару та його ціни
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+    // Конструктор для ініціалізації назви та ціни товару
+    public Product(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    // Метод для виведення інформації про товар
+    public void PrintProductInfo()
+    {
+        Console.WriteLine($"Назва товару: {Name}, Цiна: {Price} грн");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Запитуємо користувача про назву товару
+        Console.Write("Введiть назву товару: ");
+        string name = Console.ReadLine();
+
+        // Запитуємо користувача про ціну товару
+        Console.Write("Введiть цiну товару: ");
+        double price = Convert.ToDouble(Console.ReadLine());
+
+        // Створення об'єкта товару з введеними користувачем даними
+        Product product = new Product(name, price);
+
+        // Виведення інформації про товар
+        Console.WriteLine("\nIнформацiя про товар:");
+        product.PrintProductInfo();
+
+        // Очікуємо натискання клавіші для завершення програми
+        Console.WriteLine("\nНатиснiть будь-яку клавiшу для виходу...");
+        Console.ReadKey();
+    }
+}
