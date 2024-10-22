f9f93db71cb9981099790af14fcbffb1b961f3ce
Lizokbondarieva <lizokbondarieva@gmail.com>
2024-10-22 21:31:49 +0300

    laba1 punckt 4(3)

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Point3D
+{
+    // Властивості для координат точки
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+
+    // Конструктор для ініціалізації координат
+    public Point3D(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    // Метод для виведення координат точки
+    public void PrintCoordinates()
+    {
+        Console.WriteLine($"Точка має координати: X = {X}, Y = {Y}, Z = {Z}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Створення об'єкта точки
+        Point3D point = new Point3D(1.0, 2.0, 3.0);
+
+        // Виведення координат точки
+        point.PrintCoordinates();
+    }
+}
