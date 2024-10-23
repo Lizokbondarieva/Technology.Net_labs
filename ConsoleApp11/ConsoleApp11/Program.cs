daadcbe730338127f2556dd012a4c6b83cdfacf6
Lizokbondarieva <lizokbondarieva@gmail.com>
2024-10-23 18:54:13 +0300

    laba 1 punkt(4.11)

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenceSplitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Введення речення користувачем
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Введіть речення:");
+            string sentence = Console.ReadLine();
+
+            // Розбивка речення на слова за пробілом
+            string[] words = sentence.Split(' ');
+
+            // Виведення кожного слова на окремому рядку
+            Console.WriteLine("Слова у реченні:");
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            // Затримка, щоб консоль не закрилася відразу
+            Console.ReadLine();
+        }
+    }
+}
