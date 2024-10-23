f6852c9dd316b871c86d7cebca58dbeeeb371db7
Lizokbondarieva <lizokbondarieva@gmail.com>
2024-10-23 19:56:01 +0300

    laba 1 punkt(4.12)

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace JoinWords
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Запитуємо у користувача введення речення
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Введіть речення:");
+            string input = Console.ReadLine();
+
+            // Розділяємо речення на слова
+            string[] words = input.Split(' ');
+
+            // Зшиваємо всі слова без пробілів
+            string result = string.Join("", words);
+
+            // Виводимо результат
+            Console.WriteLine("Зшиті слова: " + result);
+        }
+    }
+}
