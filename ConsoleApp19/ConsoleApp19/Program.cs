951ba52d927aba1af485d5049b8a3fdcec5bcc06
Lizokbondarieva <lizokbondarieva@gmail.com>
2024-10-29 13:35:23 +0200

    laba 2 punkt(4.7)

---
+++ ConsoleApp19/ConsoleApp19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Вхідний список цілих чисел
+        List<int> numbers = new List<int> { 101, 202, 345, 12321, 909, 500, 404, 111 };
+
+        // Сортування за зменшенням останньої цифри
+        var sortedList = numbers
+            .OrderByDescending(n => n.ToString().Last())
+            .ToList();
+
+        // Виведення відсортованого списку
+        Console.WriteLine("Список, вiдсортований за зменшенням останньої цифри:");
+        sortedList.ForEach(Console.WriteLine);
+    }
+}
