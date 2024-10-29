027a3e955a53602909afd4da34e66d54e055e797
Lizokbondarieva <lizokbondarieva@gmail.com>
2024-10-29 13:41:01 +0200

    laba 2 punkt(4.8)

---
+++ ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Вхідний список цілих чисел
+        List<int> numbers = new List<int> { 101, 202, 345, 12321, 909, 500, 404, 111 };
+
+        // Пошук чисел, що починаються і закінчуються на однакову цифру за допомогою LINQ
+        var result = numbers
+            .Where(n => n.ToString().First() == n.ToString().Last())
+            .ToList();
+
+        // Виведення результату
+        Console.WriteLine("Числа, що починаються i закiнчуються однаковою цифрою:");
+        result.ForEach(Console.WriteLine);
+    }
+}
